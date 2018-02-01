ee227f484423d35fdede51268676dcc842e3034f
Rodrigo Teles <rtelesc@gmail.com>
2018-02-01 01:37:13 -0200

    criação do layout da integração

--- api.php
+++ api.php
@@ -1,5 +1,25 @@
 <?php
+	
 
+	/*a api possui um limite de 3600 requisições por hora, isso é equivalente a 1 requisição por segundo
+
+	x-ratelimit-limit: 3600 
+	x-ratelimit-remaining: 3587
+
+
+	não pode retornar todos os items em um request
+
+
+	filtros - abv_gt, abv_lt, ibu_gt, ibu_lt, ebc_gt, ebc_lt, beer_name
+
+	https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6
+
+	curl https://api.punkapi.com/v2/beers?page=2&per_page=80
+	
+
+	sem pesquisa
+	https://api.punkapi.com/v2/beers/random
+	*/
 
 	$url = "https://api.punkapi.com/v2/beers/25";
 	$ch = curl_init();
---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+
+    <title>Teste GingaOne</title>
+  </head>
+  <body>
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <h3>Integração Beers</h3>
+
+          
+
+        </div>
+      </div>
+
+      <!-- row -->
+      <div class="row">
+        <div class="col-sm-2 col-md-2 col-xs-2">
+          One of three columns
+        </div>
+        <div class="col-sm-2 col-md-2 col-xs-2">
+          One of three columns
+        </div>
+        <div class="col-sm-2 col-md-2 col-xs-2">
+          One of three columns
+        </div>
+        <div class="col-sm-2 col-md-2 col-xs-2">
+          One of three columns
+        </div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
